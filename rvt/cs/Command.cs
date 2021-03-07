5513c2b69393dfdbd88fabd8e8b975a5c233ecf2
BimManager <kirillkozlov@ymail.com>
2021-03-07 06:40:53 -0800

    tested the journaling mechanism in Revit

--- rvt/cs/Command.cs
+++ rvt/cs/Command.cs
@@ -1,9 +1,13 @@
 /*
 ** A template for a Revit external command
 ** C# Google Style Guide
+** The path to a journal 
+** "C:\Users\kkozlov\AppData\Local\Autodesk\Revit\Autodesk Revit 20XX\Journals\journa.xxxx.txt"
 */
 
 using System;
+using System.Collections.Generic;
+
 using UI = Autodesk.Revit.UI;
 using DB = Autodesk.Revit.DB;
 using ATTR = Autodesk.Revit.Attributes;
@@ -18,6 +22,9 @@ namespace Kkozlov {
 
     private UI.Result Run(UI.ExternalCommandData commandData,
                           ref string message, DB.ElementSet elements) {
+      IDictionary<string, string> logs = commandData.JournalData;
+      logs.Clear();
+      logs.Add("Kkozlov", "Hello, world!");
       UI.TaskDialog.Show("RevitApi", "Hello, world!");
       return UI.Result.Succeeded;
     }
