7a74d00416f87ba7914c10c8b1b653731d5cae2f
BimManager <kirillkozlov@ymail.com>
2021-04-16 03:27:05 -0700

    added an mvvm template

---
+++ Mvvm/Mvvm/Commands/BaseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input;
+
+namespace Mvvm.Commands {
+  public class BaseCommand : ICommand {
+    public event EventHandler CanExecuteChanged;
+    
+    public BaseCommand(Action<object> action)
+    : this(action, null) {}
+    
+    public BaseCommand(Action<object> action, Predicate<object> canExecute) {
+      _action = action;
+      _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter) {
+      return null == _canExecute ? true : _canExecute(parameter);
+    }
+
+    public void Execute(object parameter) {
+      _action(parameter);
+    }
+
+    private void OnCanExecuteChanged() {
+      if (CanExecuteChanged != null) CanExecuteChanged(this, null);
+    }
+    
+    private readonly Action<object> _action;
+    private readonly Predicate<object> _canExecute;
+  }
+}
---
+++ Mvvm/Mvvm/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel; // INotifyPropertyChanged
+using System.Windows.Input; // ICommand
+
+namespace Mvvm.ViewModels {
+  public class NavigationViewModel : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    public ICommand EmpCommand { get; private set; }
+    public ICommand DeptCommand { get; private set; }
+    
+    public object SelectedViewModel { get { return _selectedViewModel; }
+      set { _selectedViewModel = value; OnPropertyChanged("SelectedViewModel"); } }
+
+    public NavigationViewModel() {
+      EmpCommand = new Commands.BaseCommand(OpenEmp);
+      DeptCommand = new Commands.BaseCommand(OpenDept);
+    }
+
+    private void OpenEmp(object obj) {
+      SelectedViewModel = new Views.EmployeeView();
+    }
+
+    private void OpenDept(object obj) {
+      SelectedViewModel = new Views.DepartmentView();
+    }
+
+    private void OnPropertyChanged(string propName) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+    
+    private object _selectedViewModel;
+  }
+}
